d3221b7120e46a3e4d3337be9d58552a82c8a338
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-03-23 15:07:40 -0300

    folders for each week removed

--- week02/hamburger/script.js
+++
@@ -1,8 +0,0 @@
-// Store the selected elements that we are going to use
-const mainnav = document.querySelector('.navigation');
-const hambutton = document.querySelector('#menu');
-
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show');
-});
